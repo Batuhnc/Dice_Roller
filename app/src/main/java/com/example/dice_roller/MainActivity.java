d8319a08af0aff579e6db97868015c2a5c3e80b1
Batuhan <hanci.batuhan@yandex.com>
2019-10-24 11:50:13 +0100

    Implemented D-ICEBREAKER GAME and Created a method for rolling the dice.

--- app/src/main/java/com/example/dice_roller/MainActivity.java
+++ app/src/main/java/com/example/dice_roller/MainActivity.java
@@ -14,6 +14,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.*;
 public class MainActivity extends AppCompatActivity {
 
@@ -55,27 +57,32 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    int rnnum;
+    public void roll_the_dice(){
+        Random rnd = new Random();
+        rnnum = rnd.nextInt(6)+1;
+
+    }
 
         int count = 0;
+
         public void on_button_click (View view){
 
             TextView tv = (TextView) this.findViewById(R.id.num);
-            Random r = new Random();
             TextView score = this.findViewById(R.id.rslt);
             TextView guess = this.findViewById(R.id.guess);
             TextView nice = this.findViewById(R.id.nice);
             nice.setText("");
-
-            int number = r.nextInt(6) + 1;
+            roll_the_dice();
             String guessed = guess.getText().toString();
 
             try {
-                tv.setText(number);
+                tv.setText(rnnum);
                 score.setText(count);
             } catch (Exception ex) {
-                tv.setText("Number : "+Integer.toString(number));
+                tv.setText("Number : "+Integer.toString(rnnum));
                 int GN = Integer.parseInt(guessed);
-                if (GN == number) {
+                if (GN == rnnum) {
                     count++;
                     score.setText("Score : "+Integer.toString(count));
                     nice.setText("NİCE ONE");
@@ -90,6 +97,47 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+
+        public void on_button2_click(View view){
+            TextView question = (TextView) this.findViewById(R.id.question);
+            TextView tv = this.findViewById(R.id.num);
+            roll_the_dice();
+            try{
+                tv.setText(rnnum);
+            } catch(Exception ex){
+                tv.setText(("Number : "+Integer.toString(rnnum)));
+            }
+
+            switch (rnnum)
+            {
+                case 1:
+                    question.setText("If you could go anywhere in the world, where would you go?");
+                    break;
+                case 2:
+                    question.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+                    break;
+                case 3:
+                    question.setText("If you could eat only one food for the rest of your life, what would that be?");
+                    break;
+                case 4:
+                    question.setText("If you won a million dollars, what is the first thing you would buy?");
+                    break;
+                case 5:
+                    question.setText("If you could spaned the day with one fictional character, who would it be?");
+                    break;
+                case 6:
+                    question.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+                    break;
+            }
+
+        }
+
+    public void sendMessage(View view)
+    {
+        // Do something in response to button
+
+    }
+
 
 
 }
