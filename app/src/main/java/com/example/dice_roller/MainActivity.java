a3ce8741595e40793aa29650508a068b9c84eb1d
Batuhan <hanci.batuhan@yandex.com>
2019-10-24 12:23:33 +0100

    Implemented D-ICEBREAKER GAME and Created a method for rolling the dice. DisplayMessageActivity

--- app/src/main/java/com/example/dice_roller/MainActivity.java
+++ app/src/main/java/com/example/dice_roller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dice_roller;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,12 +13,14 @@ import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.*;
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,6 +138,12 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view)
     {
         // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText =   (EditText) this.findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
 
     }
 
